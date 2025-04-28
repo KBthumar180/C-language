599637e24ce081b7fd371d32f57a87c00ebfbfdf
KBthumar180 <kavya.thummar@gmail.com>
2025-04-28 18:57:51 +0530

    kt

---
+++ C-language/lab-2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main(){
+	int x,y;
+	
+printf("enter value of x:-");
+scanf("%d",&x);
+
+printf("enter value of y:-");
+scanf("%d",&y);
+
+printf("add:-%d", x+y);
+ 
+ return 0;
+}
