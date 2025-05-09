caaf085e3958fba04879cd2f58efed0276d28ce4
KBthumar180 <kavya.thummar@gmail.com>
2025-05-09 14:09:45 +0530

    lab

---
+++ C-language/project/project-4/q2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main(){
+	int i,j,no=11,n=4;
+	
+	for(i=1;i<=n;i++){
+		for(j=1;j<=i;j++){
+		printf("%d",no);
+		no++;	
+	}
+	printf("\n");			
+	}
+}
--- C-language/project/project-4/q3.c
+++ C-language/project/project-4/q3.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 main(){
 	int i,j,s;
-	for(i=1;i<=5;i++){
-		for(s=4;s>=1;s--){
-			printf("	");
-			
+	
+	for(i=5;i>=1;i--){
+		for(s=1;s<=i;s++){
+			printf(" ");
 		}
-		for(j=1;j<=5;j++){
+		for(j=i;j<=5;j++){
 			printf("%d",j);
 		}
 		printf("\n");
